fd32c94713b1c0bed3314ef00424dd1c174bd1ff
Alina Vorontsova <vorontsova-alina@mail.ru>
2022-08-09 21:59:08 +0300

    main.py update

--- main.py
+++ main.py
@@ -1,53 +1,101 @@
 import requests
-import os
-from pprint import pprint
+import datetime
+import json
+from tqdm import tqdm
 
-with open('config.txt') as config_file: # в файле построчно записаны токены для вк и я.диска
+
+with open('config.txt') as config_file: # В файле построчно записаны токены для ВК и Я.Диска
     vk_token = config_file.readline().strip()
     ya_token = config_file.readline().strip()
 
 def get_id():
-    '''Получение id пользователя, чьи фотографии надо скопировать на я.диск'''
+    '''Получение id пользователя, чьи фотографии надо скопировать на Я.Диск.'''
     user_id = input('Введите id пользователя: ')
     return user_id
 
+
 class VK:
 
-    def __init__(self, access_token, user_id, version='5.131'):
-        '''Получение параметров авторизации для запроса'''
+    def __init__(self, vk_token, user_id, version='5.131'):
+        '''Получение параметров авторизации для запроса.'''
         self.token = vk_token
-        # self.id = get_id()
+        self.id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
-    def get_photos_vk(self):
-        '''Получение всех фотографий (в данном случае аватарок, смотря что в 'album_id') пользователя'''
+    def all_photos(self, count=5):
+        '''Получение фотографий из определённого альбома пользователя.'''
         url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': user_id, 'album_id': 'profile', 'rev': 0, 'extended': 1, 'photo_sizes': 1}
+        self.count = count # По умолчанию (по условию задачи) - 5
+        params = {'owner_id': self.id, 'album_id': 'profile', 'rev': 0, 'extended': 1, 'photo_sizes': 1, 'count': self.count}
         response = requests.get(url, params={**self.params, **params}).json()
         res = response['response']['items']
         return res
 
-def get_photos_from_vk():
-    '''Получение фотографий в максимальном размере'''
-    all_photos = vk.get_photos_vk()
-    url_dict = {}
-    for photo in all_photos:
-        max_size = 0
-        file_sizes = photo['sizes']
-        for size in file_sizes:
-            file_size = size['height'] * size['width']
-            if max_size < file_size:
-                max_size = file_size
-                url_to_biggest_photo = size['url']
-                likes = photo['likes']['count']
-        url_dict[url_to_biggest_photo] = likes
-        #url_list.append(url_to_biggest_photo)
-    return url_dict
+    def biggest_photo(self):
+        '''Получение фотографий в максимальном размере.'''
+        all_photos = vk.all_photos()
+        photos_dict = {}
+        for photo in all_photos:
+            url_to_biggest_photo = photo['sizes'][-1]['url']
+            likes = photo['likes']['count']
+            date = datetime.datetime.fromtimestamp(photo['date']).strftime("%d.%m.%Y")
+            type = photo['sizes'][-1]['type']
+            photo_info = {'url': url_to_biggest_photo, 
+                'likes_count': likes, 
+                'date': date,
+                'size_type': type}
+            if likes not in photos_dict.keys():
+                photos_dict[likes] = photo_info
+            else:
+                photos_dict[f'{likes}, {date}'] = photo_info
+        return photos_dict
+
+    def json_file(self):
+        '''Создание json-файла с информацией о фото.'''
+        photos_dict = vk.biggest_photo()
+        photos_info_list = []
+        photos_info_dict = {}
+        for value in photos_dict.values():
+            photos_info_dict = {"file_name": value['likes_count'], "size": value['size_type']}
+            photos_info_list.append(photos_info_dict)
+        with open('photos_info.json', 'w') as file:
+            json.dump(photos_info_list, file, indent=4)
+            print('Succes')
 
 
-access_token_vk = vk_token
-user_id = get_id()
-vk = VK(access_token_vk, user_id)
-pprint(vk.get_photos_vk())
-#print(get_photos_from_vk())
+class Yandex:
+
+    def __init__(self, ya_token: str):
+        '''Получение параметров авторизации для запроса.'''
+        self.token = ya_token
+        self.headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {ya_token}'}
+
+    def create_new_folder(self, folder_name='Курсовая'):
+        '''Создание новой папки на Я.Диске.'''
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params = {'path': folder_name}
+        response = requests.get(url, headers=self.headers, params=params)
+        if response.status_code != 200:
+            requests.put(url, headers=self.headers, params=params)
+            print(f'Папка "{folder_name}" успешно создана.')
+        else:
+            print(f'Папка {folder_name} уже существует.')
+        return folder_name
+
+    def upload_photos(self):
+        '''Загрузка фото на Я.Диск.'''
+        url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        folder_name = ya.create_new_folder()
+        photos_dict = vk.biggest_photo()
+        for key, value in tqdm(photos_dict.items()):
+            params = {'path': f'{folder_name}/{key}', 'url': value['url'], 'overwrite': False}
+            response = requests.post(url, headers=self.headers, params=params)
+        return response
+
+
+if __name__ == '__main__':
+    user_id = get_id()
+    vk = VK(vk_token, user_id)
+    ya = Yandex(ya_token)
+    ya.upload_photos()
